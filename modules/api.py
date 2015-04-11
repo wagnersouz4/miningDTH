dad745617eca13c2dc56d57768ea0a8ed02d498a
cooper <davidontech@gmail.com>
2015-04-11 22:31:36 +1200

    flask api with cache

--- modules/api.py
+++ modules/api.py
@@ -7,6 +7,7 @@ from datetime import date
 
 app=Flask(__name__)
 
+json_cache={}
 
 def get_json(year,month,day):
     ir=IR().getInfo(date(year,month,day))
@@ -16,7 +17,11 @@ def get_json(year,month,day):
 
 @app.route('/api/miningDTH/v1.0/<int:year>/<int:month>/<int:day>',methods=['GET'])
 def get_tasks(year,month,day):
-    json=get_json(year,month,day)
+    json=json_cache.get((year,month,day),None)
+    if not json:
+        json=get_json(year,month,day)
+        json_cache[(year,month,day)]=json
+
     return jsonify(json)
 
 if __name__=='__main__':
